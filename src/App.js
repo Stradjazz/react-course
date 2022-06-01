b126732b194f720b2d7f437095569a002606f037
Stradjazz <ivodechkoff@telefonica.net>
2022-06-01 23:00:07 +0200

    final commit for now

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const API_URL = 'https://www.omdbapi.com?apikey=c032e2d7';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -27,13 +28,13 @@ const App = () => {
             <div className="search">
                 <input 
                     placeholder='Search for movies'
-                    value="Superman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img 
                     src={SearchIcon}
                     alt="search icon"
-                    onClick={() => {}}
+                    onClick={() => searchMovies(searchTerm)}
                 />
             </div>
 
@@ -45,7 +46,7 @@ const App = () => {
                                 <MovieCard movie={movie}/>
                             ))}
                         </div>
-                    ): (
+                    ) : (
                         <div className="empty">
                             <h2>No movies were found</h2>
                         </div>
